df868609fb1a090e849f5c1c469af7ee800df24c
Skrabal <skrabaldavid9@gmail.com>
2025-02-01 11:14:05 +0100

    porovnání 10

--- ifElse/Program.cs
+++ ifElse/Program.cs
@@ -10,7 +10,17 @@ namespace ifElse
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("zadejte číslo");
+            int cislo = Convert.ToInt32(Console.ReadLine());
+            if (cislo > 10)
+            {
+                Console.WriteLine("číslo je větší než 10");
+            }
+            else
+            {
+                Console.WriteLine("číslo je menší než 10");
+            }
+            Console.ReadKey();
         }
     }
 }
