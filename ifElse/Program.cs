af1391853f8b188fc5ab00b57e5d097101407e81
Skrabal <skrabaldavid9@gmail.com>
2025-02-01 11:35:54 +0100

    3 podmínky

--- ifElse/Program.cs
+++ ifElse/Program.cs
@@ -10,6 +10,7 @@ namespace ifElse
     {
         static void Main(string[] args)
         {
+            //porovnávání 10
             Console.WriteLine("zadejte číslo");
             int cislo = Convert.ToInt32(Console.ReadLine());
             if (cislo > 10)
@@ -20,6 +21,35 @@ namespace ifElse
             {
                 Console.WriteLine("číslo je menší než 10");
             }
+
+            //kladné, záporné
+            if (cislo == 0 )
+            {
+                Console.WriteLine("číslo je 0");
+            }
+            else if (cislo < 0)
+            {
+                Console.WriteLine("číslo je záporné");
+            }
+            else
+            { 
+                Console.WriteLine("číslo je kladné");
+            }
+
+            //věk
+            Console.WriteLine("zadejte svůj věk");
+            int vek = Convert.ToInt32(Console.ReadLine());
+            if (vek >= 18)
+            {
+                Console.WriteLine("je plnoletý ");
+            }
+            else
+            {
+                Console.WriteLine("neni plnoletý");
+            }
+
+
+
             Console.ReadKey();
         }
     }
